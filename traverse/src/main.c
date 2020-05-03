9bea09b1a65240e7af218c4e8ceae7ce652f29a7
thomas <thomas@MacBook-Air-de-Thomas.local>
2020-05-03 15:21:51 +0200

    first commit

---
+++ traverse/src/main.c
@@ -0,0 +1,12 @@
+/*
+ * main.c
+ *
+ *  Created on: 3 mai 2020
+ *      Author: thomas
+ */
+
+#include <stdio.h>
+int main (int argc,char ** argv) {
+	fprintf (stdout, "HELLO WORLD");
+	return 0;
+}
